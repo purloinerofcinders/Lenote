d9094d64c3603b80fe99228a9b4f4cb36b1e2f33
wtoh <wtoh.pm@gmail.com>
2016-03-22 19:18:28 +0800

    Initial Commit

---
+++ Lenote/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lenote
+//
+//  Created by Wallace Toh on 22/3/16.
+//  Copyright © 2016 Wallace Toh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
